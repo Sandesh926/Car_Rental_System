b92d9608dadf966910020412f9759dacb8cfd7fb
Sandehs <sandeshbhurtyal926@gmail.com>
2023-04-28 17:32:24 +0545

    Enusring database is created while running the application for first time

--- Backend/Car_Rental_System/Data/CarsAPIDbContext.cs
+++ Backend/Car_Rental_System/Data/CarsAPIDbContext.cs
@@ -1,17 +1,18 @@
 ﻿using Car_Rental_System.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Car_Rental_System.Data
 {
     // This class inherits from the Db COntext
-    public class CarsAPIDbContext : DbContext
+    public sealed class CarsAPIDbContext : IdentityDbContext
     {
         // Creating constructor where we are passing the options and options are being passed to base class
         public CarsAPIDbContext(DbContextOptions options) : base(options)
         {
-            //Database.EnsureCreated();
-            Database.Migrate();
+            Database.EnsureCreated();
+            //Database.Migrate();
         }
 
         // Creating the properties which acts as tables for entity framework core
