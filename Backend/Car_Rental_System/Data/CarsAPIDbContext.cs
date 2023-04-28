1df7633a98a420f2e9abd46fcc253eab504e9575
Sandehs <sandeshbhurtyal926@gmail.com>
2023-04-28 16:34:03 +0545

    TO ensure the datbase is created

--- Backend/Car_Rental_System/Data/CarsAPIDbContext.cs
+++ Backend/Car_Rental_System/Data/CarsAPIDbContext.cs
@@ -10,6 +10,7 @@ namespace Car_Rental_System.Data
         // Creating constructor where we are passing the options and options are being passed to base class
         public CarsAPIDbContext(DbContextOptions options) : base(options)
         {
+            Database.EnsureCreated();
         }
 
         // Creating the properties which acts as tables for entity framework core
