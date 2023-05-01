5d101f846261d85ccd9c17fe85b2d8dacc1db734
bijay1088 <bijaybaniya1088@gmail.com>
2023-05-01 23:49:04 +0545

    Updated rent controller to let staff rent too and approve/reject it

--- Backend/Car_Rental_System/Controllers/CarRentController.cs
+++ Backend/Car_Rental_System/Controllers/CarRentController.cs
@@ -1,8 +1,12 @@
-﻿using Car_Rental_System.Data;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Car_Rental_System.Data;
 using Car_Rental_System.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Car_Rental_System.Controllers
 {
@@ -21,27 +25,30 @@ namespace Car_Rental_System.Controllers
 
         //Add Car Rent to database 
         [HttpPost]
-        public async Task<IActionResult> AddCarRent(RentCar carRent)
+        public async Task<IActionResult> AddCarRent(RentCarRequest carRent)
         {
             var car = await dbContext.Cars.FirstOrDefaultAsync(c => c.Car_id.ToString() == carRent.Car_id);
             if (car == null)
             {
                 return BadRequest("Car does not exist in the database.");
             }
+
+            
             var customer = await dbContext.Customers.FirstOrDefaultAsync(c => c.Customer_Id.ToString() == carRent.Customer_id);
-            if (customer == null)
+            var staff = await dbContext.Staff.FirstOrDefaultAsync(c => c.Staff_Id.ToString() == carRent.Staff_id);
+            if (customer == null && staff == null)
             {
-                return BadRequest("Customer does not exist in the database.");
+                return BadRequest("User does not exist in the database.");
             }
             var carRentObj = new RentCar()
             {
                 Rent_id = Guid.NewGuid(),
                 Rent_date_From = carRent.Rent_date_From,
                 Rent_date_To = carRent.Rent_date_To,
-                Car_id = carRent.Car_id.ToString(),
-                Customer_id = customer.Customer_Id.ToString(),
+                Car_id = carRent.Car_id,
+                Staff_id = carRent.Staff_id,
+                Customer_id = carRent.Customer_id,
                 Rent_Status = carRent.Rent_Status
-                
             };
             await dbContext.RentCar.AddAsync(carRentObj);
             await dbContext.SaveChangesAsync();
@@ -69,9 +76,15 @@ namespace Car_Rental_System.Controllers
         }
 
         //Add staff name and change rent status to rented
+        //IMPORTANT: TOKEN IS REQUIRED, SEND IT IN THE HEADER
+        //TOKEN IS REQUIRED, SEND IT IN THE HEADER
+        //TOKEN IS REQUIRED, SEND IT IN THE HEADER
         [HttpPut("/accept")]
-        public async Task<IActionResult> AcceptCarRent(string car_id, string staff_id)
+        public async Task<IActionResult> AcceptCarRent(string car_id)
         {
+            string tokenString = HttpContext.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+            var staff_id = GetUserIdFromToken(tokenString);
+
             var carRentObj = await dbContext.RentCar.FirstOrDefaultAsync(c => c.Rent_id.ToString() == car_id);
             if (carRentObj == null)
             {
@@ -82,16 +95,21 @@ namespace Car_Rental_System.Controllers
             {
                 return BadRequest("Staff does not exist in the database.");
             }
-            carRentObj.Staff_id = staff_id;
-            carRentObj.Rent_Status = "Rented";
+            carRentObj.ApprovedBy = staff_id;
+            carRentObj.Rent_Status = "Accepted";
             await dbContext.SaveChangesAsync();
             return Ok(carRentObj);
         }
 
         //Change rent status to rejected
+        //IMPORTANT: TOKEN IS REQUIRED, SEND IT IN THE HEADER
+        //TOKEN IS REQUIRED, SEND IT IN THE HEADER
+        //TOKEN IS REQUIRED, SEND IT IN THE HEADER
         [HttpPut("/reject")]
-        public async Task<IActionResult> RejectCarRent(string car_id, string staff_id)
+        public async Task<IActionResult> RejectCarRent(string car_id)
         {
+            string tokenString = HttpContext.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+            var staff_id = GetUserIdFromToken(tokenString);
             var carRentObj = await dbContext.RentCar.FirstOrDefaultAsync(c => c.Rent_id.ToString() == car_id);
             if (carRentObj == null)
             {
@@ -102,12 +120,32 @@ namespace Car_Rental_System.Controllers
             {
                 return BadRequest("Staff does not exist in the database.");
             }
-            carRentObj.Staff_id = staff_id;
+            carRentObj.ApprovedBy = staff_id;
             carRentObj.Rent_Status = "Rejected";
             await dbContext.SaveChangesAsync();
             return Ok(carRentObj);
         }
 
+
+        //get user id from token
+        private string GetUserIdFromToken(string tokenString)
+        {
+            // Verify and decode the token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("AShfhajfsgahbfjhbashj.asd@shajfhjas");
+            SecurityToken validatedToken;
+            var claims = tokenHandler.ValidateToken(tokenString, new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false
+            }, out validatedToken);
+    
+            // Get the user id from the token's payload
+            var userId = claims.FindFirst(ClaimTypes.Name)?.Value;
+            return userId;
+        }
 
         
     }
